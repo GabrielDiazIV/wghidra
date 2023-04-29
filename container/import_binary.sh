9b222272a46dc8b52574e0d9139495e59beef573
ryandkutz <ryandkutz@gmail.com>
2023-04-29 17:57:11 -0500

    setup container

--- container/import_binary.sh
+++ container/import_binary.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-pip install ghidra_bridge
 python -m ghidra_bridge.install_server ./scripts
 ghidra-analyzeHeadless ./ghidra ghidra_project -import ./input/$1 -scriptPath ./scripts -postscript ghidra_bridge_server.py
